dd07a08b4e9e1a90d191d955fd1e98946cc9be01
parthipan Chandrasekeran <parthipan_chandrasekaran@yahoo.com>
2023-01-01 10:41:17 -0500

    char array

---
+++ src/TestQue/charArrayTest.java
@@ -0,0 +1,34 @@
+package TestQue;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class charArrayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String valueString = "i love coding coding love me";
+		char[] charArray = valueString.toCharArray();
+		System.out.println(charArray[1]);
+		Map<Character, Integer> sortedMap = new LinkedHashMap<>();
+		for (char letter : charArray) {
+
+			if (sortedMap.containsKey(letter) == false) {
+
+				sortedMap.put(letter, 1);
+			} else {
+				sortedMap.put(letter, sortedMap.get(letter) + 1);
+			}
+		}
+		// System.out.println(sortedMap.entrySet());
+		sortedMap.forEach((letter, count) -> {
+
+			if (count > 1 && letter != ' ') {
+
+				System.out.println(letter);
+			}
+		});
+	}
+
+}
